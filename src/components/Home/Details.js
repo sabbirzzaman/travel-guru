9278f77b459390adc3db06bda654b2afb4c691c6
sabbirzzaman <sztonmoy7@gmail.com>
2022-10-19 15:35:56 +0600

    details added home page

--- src/components/Home/Details.js
+++ src/components/Home/Details.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Details = () => {
+    const { activeSlider } = useSelector((state) => state.carousel);
+    const { place, details } = useSelector((state) =>
+        state.carousel.slider.find((slide) => slide.id === activeSlider)
+    );
+
     return (
-        <div>
-            
+        <div className="mr-14 flex flex-col items-start justify-between h-[60vh] py-14">
+            <div>
+                <h2 className="text-white text-4xl font-extrabold mb-4">
+                    {place}
+                </h2>
+                <p className="text-white">{details.substring(0, 280)} ...</p>
+            </div>
+
+            <button className="bg-[#2977c4] text-white py-2 px-7 rounded-md font-medium text-base transition-all duration-300 hover:bg-[#3498db]">
+                Book Now
+            </button>
         </div>
     );
 };
--- src/features/carousel/carouselSlice.js
+++ src/features/carousel/carouselSlice.js
@@ -6,24 +6,32 @@ const initialState = {
             id: 0,
             img: 'https://i.ibb.co/9YnR2ZV/img-1.jpg',
             place: 'Sundarban',
+            details:
+                "Sundarbans is a mangrove area in the delta formed by the confluence of the Padma, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans the area from the Baleswar River in Bangladesh's division of Khulna to the Hooghly River in India's state of West Bengal.",
         },
         {
             id: 1,
             img: 'https://i.ibb.co/Jn2pS45/img-2.jpg',
             place: "Cox's Bazar",
+            details:
+                'Cox’s Bazar is a town on the southeast coast of Bangladesh. It’s known for its very long, sandy beachfront, stretching from Sea Beach in the north to Kolatoli Beach in the south. Aggameda Khyang monastery is home to bronze statues and centuries-old Buddhist manuscripts. South of town, the tropical rainforest of Himchari National Park has waterfalls and many birds. North, sea turtles breed on nearby Sonadia Island.',
         },
         {
             id: 2,
             img: 'https://i.ibb.co/zX8Y2vN/img-3.jpg',
             place: 'Sajek Valley',
+            details:
+                'Sajek Tripuri Valley is one of the most popular tourist spots in Bangladesh situated among the hills of the Kasalong range of mountains in Sajek union, Baghaichhari Upazila in Rangamati District. The valley is 2,000 feet above sea level. Sajek Tripuri Valley is known as the Queen of Hills & Roof of Rangamati.',
         },
         {
             id: 3,
             img: 'https://i.ibb.co/2Pr136D/img-4.jpg',
             place: 'Bandarban',
+            details:
+                'Bandarban town is the hometown of the Bohmong Chief. Tripadvisor has 621 reviews of Bandarban Hotels, Attractions, and Restaurants making it your best Bandarban resource. Bandarban is regarded as one of the most attractive travel destinations in Bangladesh. It also is the administrative headquarters of Bandarban district, which has turned into one of the most exotic tourist attractions in Bangladesh.',
         },
     ],
-    activeSlider: 0
+    activeSlider: 0,
 };
 
 const carouselSlice = createSlice({
